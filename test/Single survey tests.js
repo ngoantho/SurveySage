4c4fb73a2d92f49a47295f2f4fa52e69272cc6e5
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-21 12:43:34 -0800

    single survey test: survey should be an object

--- test/Single survey tests.js
+++ test/Single survey tests.js
@@ -23,29 +23,33 @@ describe("API call for /survey/<number>", () => {
 
 describe('Test Survey Object result', function () {
   //	this.timeout(15000);
-  
-    var requestResult;
-    var response;
-       
-      before(function (done) {
-          chai.request("http://localhost:8080")
-        .get("/app/survey/1")
-        .end(function (err, res) {
-          requestResult = res.body;
-          response = res;
-                  expect(err).to.be.null;
-                  expect(res).to.have.status(200);
-          done();
-        });
-          });
-      
-          it('Should contain the values _id, surveyId, name, description, owner, and status as expected for Survey 1', function() {
-            expect(response.body).to.have.property('surveyId', 1).that.is.a('number');
-            expect(response.body).to.have.property('name','Customer Feedback Survey');
-            expect(response.body).to.have.property('description','Survey to gather customer feedback on our products and services.');
-            expect(response.body).to.have.property('owner', 'VSomwanshi');
-            expect(response.body).to.have.property('status','active');
-    });
-    
+
+  var requestResult;
+  var response;
+
+  before(function (done) {
+    chai.request("http://localhost:8080")
+      .get("/app/survey/1")
+      .end(function (err, res) {
+        requestResult = res.body;
+        response = res;
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
+
+  it('Should contain the values _id, surveyId, name, description, owner, and status as expected for Survey 1', function () {
+    expect(response.body).to.have.property('surveyId', 1).that.is.a('number');
+    expect(response.body).to.have.property('name', 'Customer Feedback Survey');
+    expect(response.body).to.have.property('description', 'Survey to gather customer feedback on our products and services.');
+    expect(response.body).to.have.property('owner', 'VSomwanshi');
+    expect(response.body).to.have.property('status', 'active');
+  });
+
+  it('Should be an object and not a string', function () {
+    expect(response.body).to.be.a('object');
+    expect(response.body).to.not.be.a.string;
+  })
+});
 

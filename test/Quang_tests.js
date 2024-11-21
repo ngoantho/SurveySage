252c9e2525b27bf60b192d69b2ddaf2137f20de4
quangtran0603 <qtran1@seattleu.edu>
2024-11-20 20:42:18 -0800

    Add test for Survey lists

---
+++ test/Quang_tests.js
@@ -0,0 +1,37 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var async = require('async');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var http = require('http');
+chai.use(chaiHttp);
+
+describe('Test Survey lists result', function () {
+//	this.timeout(15000);
+
+	var requestResult;
+	var response;
+		 
+    before(function (done) {
+        chai.request("http://localhost:8080")
+			.get("/app/surveys")
+			.end(function (err, res) {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+				done();
+			});
+        });
+    
+	it('Should return an array object with 3 or more objects', function (){
+		expect(response).to.have.status(200); //return status is 200
+		expect(response.body).to.be.an('array') //test that return value is an array
+		expect(response.body).to.have.lengthOf.at.least(3);
+		expect(response).to.have.headers;
+	});
+	
+});

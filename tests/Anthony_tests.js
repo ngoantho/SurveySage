7e727bc1ee84a26b59fd978247c6febc9393a326
Anthony Ngo <anthony.ngoxd@outlook.com>
2024-11-20 19:25:52 -0800

    Anthony: push my tests

---
+++ tests/Anthony_tests.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var async = require('async');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var http = require('http');
+chai.use(chaiHttp);
+
+describe('API call for /survey/<number>', () => {
+  it('Should return null for survey 123', () => {
+    chai
+      .request('http://localhost:8080')
+      .get('/app/survey/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res).to.be.null
+      })
+  })
+})

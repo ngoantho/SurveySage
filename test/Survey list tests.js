d7e05a4c08b7b3a2ba9a18df60738ba5f4a847dd
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-21 12:36:28 -0800

    survey lists: each survey should have expected attributes and be an object

--- test/Survey list tests.js
+++ test/Survey list tests.js
@@ -7,31 +7,44 @@ var expect = chai.expect;
 var should = chai.should();
 
 var http = require('http');
+const { query } = require('express');
 chai.use(chaiHttp);
 
 describe('Test Survey lists result', function () {
-//	this.timeout(15000);
+	//	this.timeout(15000);
 
 	var requestResult;
 	var response;
-		 
-    before(function (done) {
-        chai.request("http://localhost:8080")
+
+	before(function (done) {
+		chai.request("http://localhost:8080")
 			.get("/app/surveys")
 			.end(function (err, res) {
 				requestResult = res.body;
 				response = res;
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
 				done();
 			});
-        });
-    
-	it('Should return an array object with 3 or more objects', function (){
+	});
+
+	it('Should return an array object with 3 or more objects', function () {
 		expect(response).to.have.status(200); //return status is 200
 		expect(response.body).to.be.an('array') //test that return value is an array
 		expect(response.body).to.have.lengthOf.at.least(3);
 		expect(response).to.have.headers;
 	});
-	
+
+	it('Each survey should have a expected types', () => {
+		response.body.every(survey => expect(survey).to.have.property('surveyId').and.to.be.a('number'))
+		response.body.every(survey => expect(survey).to.have.property('name').and.to.be.a.string)
+		response.body.every(survey => expect(survey).to.have.property('description').and.to.be.a.string)
+		response.body.every(survey => expect(survey).to.have.property('owner').and.to.be.a.string)
+		response.body.every(survey => expect(survey).to.have.property('status').and.to.be.a.string)
+	})
+
+	it('Survey list should be an array of objects', () => {
+		expect(response.body).to.not.be.a.string;
+		response.body.every(survey => expect(survey).to.be.an('object'))
+	})
 });

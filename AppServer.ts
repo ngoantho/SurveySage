6f7d16c2ec65258956479a920f78f3c4830c8d53
Anthony Ngo <ngoanthony@seattleu.edu>
2024-12-05 17:03:07 -0800

    update AppServer to use atlas' mongodb+srv schema

--- AppServer.ts
+++ AppServer.ts
@@ -6,7 +6,8 @@ dotenv.config();
 const port = process.env.PORT;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const mongoDBConnection = 'mongodb://' + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
+const schema = process.env.DB_SCHEMA;
+const mongoDBConnection = schema + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
 console.log("server db connection URL " + mongoDBConnection);
 
 let server: any = new App(mongoDBConnection).expressApp;

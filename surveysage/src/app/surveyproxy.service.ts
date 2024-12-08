f980422a993db20b7028bc2bab2d1eb0a76ad27b
quangtran0603 <qtran1@seattleu.edu>
2024-12-07 23:25:54 -0800

    Changing surveyproxy

--- surveysage/src/app/surveyproxy.service.ts
+++ surveysage/src/app/surveyproxy.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { IQuestionModel, ISurveyModel, IAnalysisModel } from './interfaces';
+import { IQuestionModel, ISurveyModel, IAnalysisModel, ISurvey } from './interfaces';
 
 @Injectable({
   providedIn: 'root',

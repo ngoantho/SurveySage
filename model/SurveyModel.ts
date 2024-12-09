3877ee1ec0bfd66ede842b830bb7f14edf84127f
quangtran0603 <qtran1@seattleu.edu>
2024-12-09 13:23:13 -0800

    Adjusting to work with SSO for response

--- model/SurveyModel.ts
+++ model/SurveyModel.ts
@@ -30,71 +30,71 @@ class SurveyModel extends CommonModel<ISurvey> {
   }
 
   public async getAllSurveys(response: Response, id: number) {
-    let query = this.model.find({userId: id})
+    let query = this.model.find({ userId: id })
     try {
       const surveys = await query.exec()
       response.json(surveys)
-    } catch(e) {
+    } catch (e) {
       response.send(e)
     }
   }
 
   public async getSurveyById(response: Response, surveyId: number, userId: number) {
-    let query = this.model.findOne({surveyId, userId})
+    let query = this.model.findOne({ surveyId, userId })
     try {
       const survey = await query.exec()
       response.json(survey)
-    } catch(e) {
+    } catch (e) {
       response.send(e)
     }
   }
 
-  
 
-  public async retrieveSurveyCount(response:any) {
+
+  public async retrieveSurveyCount(response: any) {
     console.log("retrieve Survey Count ...");
     var query = this.model.estimatedDocumentCount();
     try {
-        const numberOfLists = await query.exec();
-        console.log("numberOfSurvey: " + numberOfLists);
-        response.json(numberOfLists);
+      const numberOfLists = await query.exec();
+      console.log("numberOfSurvey: " + numberOfLists);
+      response.json(numberOfLists);
     }
     catch (e) {
-        console.error(e);
+      console.error(e);
     }
-}
-
-//RETURN SURVEY INSTEAD OF RESPONSE
-public async returnSurveyById( id: number) {
-  let query = this.model.findOne({surveyId: id})
-  try {
-    const survey = await query.exec()
-    return survey;
-  } catch(e) {
-    throw new Error("Failed to retrieve survey");
   }
-}
 
-//TEST PURPOSE
-
-public async getAllSurveys_unprotection(response: Response) {
-  let query = this.model.find({})
-  try {
-    const surveys = await query.exec()
-    response.json(surveys)
-  } catch(e) {
-    response.send(e)
+  //RETURN SURVEY INSTEAD OF RESPONSE
+  public async returnSurveyById(id: number) {
+    let query = this.model.findOne({ surveyId: id })
+    try {
+      const survey = await query.exec()
+      return survey;
+    } catch (e) {
+      throw new Error("Failed to retrieve survey");
+    }
   }
-}
 
-public async getSurveyById_unprotection(response: Response, id: number) {
-  let query = this.model.findOne({surveyId: id})
-  try {
-    const survey = await query.exec()
-    response.json(survey)
-  } catch(e) {
-    response.send(e)
+  //TEST PURPOSE
+
+  public async getAllSurveys_unprotection(response: Response) {
+    let query = this.model.find({})
+    try {
+      const surveys = await query.exec()
+      response.json(surveys)
+    } catch (e) {
+      response.send(e)
+    }
   }
-}
+
+  public async getSurveyById_unprotection(response: Response, id: number) {
+    let query = this.model.findOne({ surveyId: id })
+    try {
+      const survey = await query.exec()
+      response.json(survey)
+    } catch (e) {
+      response.send(e)
+    }
+  }
 }
 export { SurveyModel };
